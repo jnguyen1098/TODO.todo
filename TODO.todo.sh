4f756c6a0c1c621440e73f771de1911459a276b4
jnguyen1098 <jnguyen1098@gmail.com>
2021-03-28 18:00:27 -0400

    refactor

--- TODO.todo.sh
+++ TODO.todo.sh
@@ -1,61 +1,74 @@
 #!/usr/bin/env sh
 
-todo()
-{
-    TODOFILE="TODO.todo"
-    TODO_EDITOR="$EDITOR"
+TODO_FILE="TODO.todo"
+TODO_EDITOR="$EDITOR"
+EMPTY_TODO_LIST_MESSAGE="Your TODO.todo list is empty! Congrats!"
 
+print_todo_file() {
+    if [ $(wc -l < $1) -eq 0 ]; then
+        echo "$EMPTY_TODO_LIST_MESSAGE"
+    else
+        cat -n "$1"
+    fi
+}
+
+print_todo_help() {
+    echo "todo         - print $TODO_FILE"
+    echo "todo <text>  - add <text> to bottom of $TODO_FILE"
+    echo "todo edit    - edit the $TODO_FILE directly"
+    echo "todo del <n> - delete the <n>th entry off $TODO_FILE"
+    echo 'todo pt      - pop top TODO entry'
+    echo 'todo pb      - pop bottom TODO entry'
+}
+
+todo() {
     if [ "$1" = "help" ]; then
-        echo "todo         - print $TODOFILE"
-        echo "todo <text>  - add <text> to bottom of $TODOFILE"
-        echo "todo edit    - edit the $TODOFILE directly"
-        echo "todo del <n> - delete the <n>th entry off $TODOFILE"
-        echo 'todo pt      - pop top TODO entry'
-        echo 'todo pb      - pop bottom TODO entry'
+        print_todo_help
         return
     fi
 
-    if [ -d "$TODOFILE" ]; then
-        echo "$TODOFILE is a directory. Quitting."
+    if [ -d "$TODO_FILE" ]; then
+        echo "$TODO_FILE is a directory. Quitting."
         return
-    elif [ ! -f "$TODOFILE" ]; then
-        echo "Creating $(pwd)/$TODOFILE"
-        touch "$TODOFILE"
+    elif [ ! -f "$TODO_FILE" ]; then
+        echo "Creating $(pwd)/$TODO_FILE"
+        touch "$TODO_FILE"
     fi
 
     case "$#" in
         0)
-            cat -n "$TODOFILE"
+            print_todo_file "$TODO_FILE"
             ;;
         1)
             case "$1" in
                 "edit")
-                    "$TODO_EDITOR" "$TODOFILE"
+                    "$TODO_EDITOR" "$TODO_FILE"
                     ;;
                 "pt")
-                    sed -i '1d' "$TODOFILE"
-                    cat -n "$TODOFILE"
+                    sed -i '1d' "$TODO_FILE"
+                    print_todo_file "$TODO_FILE"
                     ;;
                 "pb")
-                    sed -i '$d' "$TODOFILE"
-                    cat -n "$TODOFILE"
+                    sed -i '$d' "$TODO_FILE"
+                    print_todo_file "$TODO_FILE"
                     ;;
                 *)
-                    echo "$1" >> "$TODOFILE"
-                    cat -n "$TODOFILE"
+                    echo "$1" >> "$TODO_FILE"
+                    print_todo_file "$TODO_FILE"
                     ;;
             esac
             ;;
         2)
             case "$1" in
                 "del")
-                    sed -i "$2d" "$TODOFILE"
-                    cat -n "$TODOFILE"
+                    sed -i "$2d" "$TODO_FILE"
+                    print_todo_file "$TODO_FILE"
                     ;;
             esac
             ;;
         *)
-            echo "Too many arguments"
+            print_todo_help
             ;;
     esac
 }
+
